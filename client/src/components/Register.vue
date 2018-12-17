18179b73ed460345c77b5c39c5cf771f8e3e819f
DESKTOP-61LCS5U\schmi <sebastian.s3@gmx.de>
2018-12-17 20:46:48 +0100

    changes

---
+++ client/src/components/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1> Register </h1>
+</template>
+
+<script>
+import AuthenticationService from '@services/AuthenticationService'
+export default {
+    name: 'Register',
+  data(){
+      return{
+
+      };
+  },
+  methods:  {
+      register(){
+          AuthenticationService.register({
+              email: "acvby",
+              password: "eweqe"
+          })
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+console.log("Works!")
+const express = require('express');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const app = express()
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json())
+
+app.post('/register', (req, res) => (
+    res.send({
+    "msg":`lol you are ${req.body.email}`
+    })
+))
+
+app.listen(process.env.PORT || 8081)
