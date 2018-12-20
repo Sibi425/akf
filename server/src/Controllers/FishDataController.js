b3a02bb7e4ae12951201f29f72b3088f9d1ff8ed
Sibi425 <32645014+Sibi425@users.noreply.github.com>
2018-12-20 12:10:58 +0100

    Update FishDataController.js

--- server/src/Controllers/FishDataController.js
+++ server/src/Controllers/FishDataController.js
@@ -1,11 +1,11 @@
 const config = require('../config/config')
 const path = require('path')
-const fishDataFile = path.resolve(__dirname, '../' + config.fishes)
+//const fishDataFile = path.resolve(__dirname, '../' + config.fishes)
 
 module.exports = {
     getFishData(req, res){
 
-        res.sendFile(fishDataFile);
+        res.sendFile(config.fishes);
 
     }
 }
